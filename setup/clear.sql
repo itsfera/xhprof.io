4b3aaf7288e54a9e75d32afad5cdb7f287be7b83
itsfera <info@it-sfera.ru>
2018-11-29 13:49:43 +0300

    Update clear.sql

--- setup/clear.sql
+++ setup/clear.sql
@@ -1,7 +1,9 @@
-TRUNCATE TABLE `calls`;
+# Удаляет результаты из таблиц
+
 TRUNCATE TABLE `players`;
 TRUNCATE TABLE `request_data`;
 TRUNCATE TABLE `request_hosts`;
-TRUNCATE TABLE `request_methods`;
-TRUNCATE TABLE `request_uris`;
-TRUNCATE TABLE `requests`;
+TRUNCATE TABLE `calls`;
+delete from `request_uris`;
+delete from `requests`;
+delete from `request_methods`;
